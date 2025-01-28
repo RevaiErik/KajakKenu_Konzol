e9087fcf33c141d6f4241ef32237d90186b60964
Révai Erik <revaierik100@gmail.com>
2025-01-28 10:01:31 +0100

    Add project files.

---
+++ Kolcsonzes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KajakKenu
+{
+    internal class Kolcsonzes
+    {
+        public string Nev { get; set; }
+        public int Hajoid { get; set; }
+        public string Hajotipus { get; set; }
+        public int Szemelyekszama { get; set; }
+        public int Elvitelora { get; set; }
+        public int Elvitelperc { get; set; }
+        public int Visszahozatalora { get; set; }
+        public int Visszahozatalperc { get; set; }
+
+        public Kolcsonzes(string row)
+        {
+            string[] data = row.Split(';');
+            Nev = data[0];
+            Hajoid = int.Parse(data[1]);
+            Hajotipus = data[2];
+            Szemelyekszama = int.Parse(data[3]);
+            Elvitelora = int.Parse(data[4]);
+            Elvitelperc = int.Parse(data[5]);
+            Visszahozatalora = int.Parse(data[6]);
+            Visszahozatalperc = int.Parse(data[7]);
+        }
+    }
+}
